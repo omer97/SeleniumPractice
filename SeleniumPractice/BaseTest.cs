ebe318c156f7c68f38cd9557f99d6135192c7995
DESKTOP-HFGNDAP\CBM <fasihbaig85@gmail.com>
2022-09-16 20:55:01 +0500

    Added Base class with NUNIT annotation Onetimesetup and onetimeteardown

---
+++ SeleniumPractice/BaseTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SeleniumPractice
+{
+    public class BaseTest
+    {
+        public IWebDriver webDriver;
+
+        [OneTimeSetUp]
+        public void Open()
+        {
+            webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            webDriver.Url = "https://www.facebook.com";
+        }
+
+        [OneTimeTearDown]
+        public void Quit()
+        {
+            webDriver.Quit();
+        }
+    }
+}
--- SeleniumPractice/FacebookLogin.cs
+++ SeleniumPractice/FacebookLogin.cs
@@ -3,24 +3,25 @@ namespace SeleniumPractice
     using OpenQA;
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
-    public class Tests
+    public class Tests : BaseTest
     {
+
+
         [SetUp]
         public void Setup()
         {
+            
         }
 
         [Test]
         public void Test1()
         {
-            IWebDriver webDriver = new ChromeDriver();
-            webDriver.Url = "https://www.facebook.com/";
+           
+           // webDriver.Url = "https://www.facebook.com/";
 
             //send email
             IWebElement emailElement = webDriver.FindElement(By.Id("email"));
             emailElement.SendKeys("EnterYourEmail");
-
-
 
             //send password
             IWebElement passElement = webDriver.FindElement(By.Id("pass"));
@@ -31,7 +32,8 @@ namespace SeleniumPractice
 
 
             //comment the following line if you want the browser to stay instead of closing it.
-            webDriver.Close();
+          //  webDriver.Close();
         }
+
     }
 }
